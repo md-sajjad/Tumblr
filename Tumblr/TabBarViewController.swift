1b8b1ea992283eba89fe8235147c190eacc6531d
md.sajjad <md.sajjad@gmail.com>
2015-10-12 23:55:50 -0700

    Assignment #4:  Finished till Section #8
    8.  Implement TabBarViewController behavior
    Connect the tab bar buttons to a shared action function. In that
    function, add the view of the selected view controller to the
    contentView.
    It's useful to track what the currently selected button and view
    controller is in a property, so you can unselect the button or remove
    the view controller's view when another button is selected.
    Guide: Creating a Custom Tab Bar

--- Tumblr/TabBarViewController.swift
+++ Tumblr/TabBarViewController.swift
@@ -10,18 +10,73 @@ import UIKit
 
 class TabBarViewController: UIViewController {
 
+    @IBOutlet weak var contentView: UIView!
+    
+    var homeViewCtrlr: UIViewController!
+    var searchViewCtrlr: UIViewController!
+    var accountViewCtrlr: UIViewController!
+    var trendingViewCtrlr: UIViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        homeViewCtrlr = storyboard.instantiateViewControllerWithIdentifier("HomeViewController")
+        searchViewCtrlr = storyboard.instantiateViewControllerWithIdentifier("SearchViewController")
+        accountViewCtrlr = storyboard.instantiateViewControllerWithIdentifier("AccountViewController")
+        trendingViewCtrlr = storyboard.instantiateViewControllerWithIdentifier("TrendingViewController")
+        
         // Do any additional setup after loading the view.
     }
 
+    func handleTabBarButtonPress(button: UIButton, viewCtrlr: UIViewController) {
+        if previousButton != nil {
+            previousButton.selected = false
+        }
+        if currentViewCtrlr != nil {
+            currentViewCtrlr.willMoveToParentViewController(nil)
+            currentViewCtrlr.view.removeFromSuperview()
+            currentViewCtrlr.removeFromParentViewController()
+        }
+        button.selected = true
+        addChildViewController(viewCtrlr)
+        contentView.addSubview(viewCtrlr.view)
+        viewCtrlr.didMoveToParentViewController(self)
+        viewCtrlr.view.frame = contentView.bounds
+        previousButton = button
+        currentViewCtrlr = viewCtrlr
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
+    var previousButton: UIButton!
+    var currentViewCtrlr : UIViewController!
+    
+    
+    @IBAction func onHomeButton(button: UIButton) {
+        handleTabBarButtonPress(button, viewCtrlr: homeViewCtrlr)
+    }
+
+    
+    @IBAction func onSearchButton(button: UIButton) {
+        handleTabBarButtonPress(button, viewCtrlr: searchViewCtrlr)
+    }
+
+    
+    @IBAction func onAccountButton(button: UIButton) {
+        handleTabBarButtonPress(button, viewCtrlr: accountViewCtrlr)
+    }
+    
+    
+    @IBAction func onTrendingButton(button: UIButton) {
+        handleTabBarButtonPress(button, viewCtrlr: trendingViewCtrlr)
+    }
+    
     /*
     // MARK: - Navigation
 
